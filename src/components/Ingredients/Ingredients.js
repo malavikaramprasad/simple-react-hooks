ca7967b6aa8595718705760158b0542b97b2d5ea
Malavika Ramprasad <31526040+malavikaramprasad@users.noreply.github.com>
2020-06-14 13:48:40 -0700

    Add hooks to Ingredients.js

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
 const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredient = (newIngredients) => {
+    setIngredients(prevIngredients =>
+      [...prevIngredients, { id: Math.random().toString(), ...newIngredients}]
+    );
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredient} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
