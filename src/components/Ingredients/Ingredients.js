d7aa086f5ad1aa9a50c7ecebfed77d7be6a18a49
Malavika Ramprasad <31526040+malavikaramprasad@users.noreply.github.com>
2020-07-16 20:31:51 -0700

    Add logic for removing ingredient + store them in firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,8 +7,22 @@ const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
   const addIngredient = (newIngredients) => {
-    setIngredients(prevIngredients =>
-      [...prevIngredients, { id: Math.random().toString(), ...newIngredients}]
+    fetch("https://react-hooks-update-3673e.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify({ newIngredients}),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+        setIngredients(prevIngredients =>
+          [...prevIngredients, { id: responseData.name, ...newIngredients }]
+        );
+    });
+  }
+
+  const removeIngredient = (ingredientId) => {
+    setIngredients(prevIngredients => 
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
     );
   }
 
@@ -19,7 +33,7 @@ const Ingredients = () => {
       <section>
         <Search />
         {/* Need to add list here! */}
-        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredient} />
       </section>
     </div>
   );
